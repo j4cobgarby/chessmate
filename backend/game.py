0c41faf06b3aa90543dba6dee31fd965cfbed698
jacob G <j4cobgarby@gmail.com>
2022-03-10 14:11:38 +0000

    fixed pawn

--- backend/game.py
+++ backend/game.py
@@ -21,7 +21,7 @@ class Board:
 
     def __str__(self):
         ret = ""
-        for r in self.board:
+        for r in reversed(self.board):
             for p in r:
                 ret += str(p) if p != None else "_ "
             ret += "\n"
@@ -29,6 +29,9 @@ class Board:
 
     def __getitem__(self, key):
         return self.board[key]
+
+    def __len__(self):
+        return len(self.board)
 
     def in_board(self, r, c):
         return r >= 0 and r < len(self.board) and c >= 0 and c < len(self.board[0])
@@ -41,26 +44,26 @@ class Pawn(Piece):
     def get_moves(self, board, r, c):
         # Add promotion and en passant
         ret = []
-        if self.colour == 0:  # Black
-            if r + 1 < len(board):
+        if self.colour == 1:  # White
+            if r + 1 < len(board) and board[r+1][c] is None:
                 ret = [(r + 1, c)]
                 a = board[r + 1][c - 1]
                 b = board[r + 1][c + 1]
 
-                if c - 1 >= 0 and a != None and a.colour != self.colour:
+                if c - 1 >= 0 and a is not None and a.colour != self.colour:
                     ret.append((r + 1, c - 1))
                 if c + 1 < len(board[0]) and b != None and b.colour != self.colour:
                     ret.append((r + 1, c + 1))
 
-        else:  # White
+        else:  # Black
             if r - 1 >= 0:
                 ret = [(r - 1, c)]
                 a = board[r - 1][c - 1]
                 b = board[r - 1][c + 1]
 
-                if c - 1 >= 0 and a != None and a.colour != self.colour:
+                if c - 1 >= 0 and a is not None and a.colour != self.colour:
                     ret.append((r - 1, c - 1))
-                if c + 1 < len(board[0]) and b != None and b.colour != self.colour:
+                if c + 1 < len(board[0]) and b is not None and b.colour != self.colour:
                     ret.append((r - 1, c + 1))
 
         return ret
@@ -96,7 +99,7 @@ class Rook(Piece):
                     break
 
         # Above the rook:
-        if board.in_board(r + 1, c, len(board), len(board[0])):
+        if board.in_board(r + 1, c):
             for y in range(r + 1, len(board)):
                 if board[y][c] is None:
                     valid_squares.append((y,c))
@@ -107,7 +110,7 @@ class Rook(Piece):
                     break
 
         # Below the rook:
-        if board.in_board(r - 1, c, len(board), len(board[0])):
+        if board.in_board(r - 1, c):
             for y in range(r - 1, -1, -1):
                 if board[y][c] is None:
                     valid_squares.append((y,c))
